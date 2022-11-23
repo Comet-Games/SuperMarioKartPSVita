7b30401434a99bb9030af54494f6784cc73472da
juwho123 <114698027+Comet-Games@users.noreply.github.com>
2022-11-23 16:30:49 +0000

    Build 3

--- Assets/Scripts/KartController.cs
+++ Assets/Scripts/KartController.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class KartController : MonoBehaviour {
 
-	[SerializeField] float acceleration, gravity, steering, drifting;
+	[SerializeField] float driftingf, groundCheckHeight;
 	[SerializeField] Rigidbody sphere;
 	[SerializeField] Animator spriteVisual;
-	public bool testing;
 
-	private float speed, currentSpeed;
+	[Header("Kart Stuff")]
+	[SerializeField] private float speed, currentSpeed;
 	private float rotate, currentRotation;
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		transform.position = sphere.transform.position;
+	private int driftDir;
+	public float driftPower;
+	public int driftMode = 0;
+	public bool first, second, third;
+	public Color c;
 
+	[Header("Bools")]
+	public bool drifting;
+
+	[Header("Parameters")]
+	public float acceleration;
+	public float steering;
+	public float gravity;
+	public float boost;
+
+    [Header("Testing")]
+    public bool testing;
+	public bool grounded;
+
+    // Update is called once per frame
+    void Update () 
+	{
+		transform.position = sphere.transform.position;//Follow sphere
+        #region - Acceleration -
+        //Acceleration
         if (Input.GetButton("Fire1") || (Input.GetButton("Cross")))
 		{
 			speed = acceleration;
 		}
+        #endregion
 
-		if(testing == false)
+        #region - Steering -
+        //Steering
+        if (testing == false)
 		{
 			if((Input.GetAxis("Left Stick Horizontal") != 0f))
 			{
@@ -30,7 +52,6 @@ public class KartController : MonoBehaviour {
 				float amount = Mathf.Abs(Input.GetAxis("Left Stick Horizontal"));
 				Steer(dir, amount);
 			}
-            spriteVisual.SetFloat("Horizontal", Input.GetAxis("Left Stick Horizontal"));
         }
         if(testing == true)
 		{
@@ -40,18 +61,74 @@ public class KartController : MonoBehaviour {
                 float amount = Mathf.Abs(Input.GetAxis("Horizontal"));
                 Steer(dir, amount);
             }
-
-            spriteVisual.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         }
+
+		Visuals();
 
 		currentSpeed = Mathf.SmoothStep(currentSpeed, speed, 12f * Time.deltaTime);
 		speed = 0f;
 		currentRotation = Mathf.Lerp(currentRotation, rotate, 4f * Time.deltaTime);
 		rotate = 0f;
-	}
+        #endregion
 
-	private void FixedUpdate()
+        #region - Drifting -
+        if ((Input.GetButtonDown("Right Shoulder") || Input.GetKey(KeyCode.UpArrow)) && !drifting && ((Input.GetAxis("Left Stick Horizontal") != 0) || (Input.GetAxis("Horizontal")) != 0))
+		{
+			drifting = true;
+			if(testing)
+			{
+				driftDir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+			}
+			if(!testing)
+			{
+                driftDir = Input.GetAxis("Left Stick Horizontal") > 0 ? 1 : -1;
+            }
+		}
+
+		if (drifting)
+		{
+			float control;
+			float powerControl;
+			if(testing)
+			{
+				control = (driftDir == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+				powerControl = (driftDir == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .2f, 1) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 1, .2f);
+				Steer(driftDir, control);
+                driftPower += powerControl;
+            }
+            if (!testing)
+			{
+                control = (driftDir == 1) ? ExtensionMethods.Remap(Input.GetAxis("Left Stick Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Left StickHorizontal"), -1, 1, 2, 0);
+                powerControl = (driftDir == 1) ? ExtensionMethods.Remap(Input.GetAxis("Left Stick Horizontal"), -1, 1, .2f, 1) : ExtensionMethods.Remap(Input.GetAxis("Left Stick Horizontal"), -1, 1, 1, .2f);
+				Steer(driftDir, control);
+				driftPower += powerControl;
+            }
+
+		}
+
+		if(Input.GetButtonUp("Right Shoulder") || Input.GetKeyUp(KeyCode.UpArrow) && drifting)
+		{
+			StartCoroutine(Boost(0.2f));
+			drifting = false;// (Boosting goes here)
+		}
+        #endregion
+
+		if(!grounded)
+		{
+			gravity = 1;
+			acceleration = 1;
+		}
+		if(grounded)
+		{
+			gravity = 20;
+			acceleration = 15;
+		}
+    }
+
+    private void FixedUpdate()
 	{
+		GroundCheck();
+
 		sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
 
 		sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
@@ -61,14 +138,50 @@ public class KartController : MonoBehaviour {
 
 	void Steer(int dir, float amount)
 	{
-        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetButton("Right Shoulder")))
+		/*if ((Input.GetKey(KeyCode.UpArrow) || (Input.GetButton("Right Shoulder"))))
         {
-            rotate = (drifting * dir) * amount;
+            drifting = true;
+            rotate = (driftingf * dir) * amount;
         }
 		else
 		{
-			rotate = (steering * dir) * amount;
-
-		}
+            drifting = false;
+            rotate = (steering * dir) * amount;
+		}*/
+		rotate = (steering * dir) * amount;
 	}
+
+	void GroundCheck()
+	{
+		if(Physics.Raycast(sphere.transform.position, Vector3.down, groundCheckHeight))
+		{
+			grounded = true;
+		}
+		else
+		{
+			grounded = false;
+		}
+        Debug.DrawRay(sphere.transform.position, Vector3.down * groundCheckHeight, Color.green);
+    }
+
+	IEnumerator Boost(float time)
+	{
+		drifting = false;
+		acceleration -= boost;
+		yield return new WaitForSeconds(time);
+		acceleration += boost;
+	}
+
+	void Visuals()
+	{
+		if(testing)
+		{
+            spriteVisual.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        }
+		if(!testing)
+		{
+            spriteVisual.SetFloat("Horizontal", Input.GetAxis("Left Stick Horizontal"));
+        }
+	}
+
 }
